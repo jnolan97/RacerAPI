4f837db737b059bc55b7dae1bcf4150a1794a829
jnolan97 <johnmnolan97@gmail.com>
2020-06-11 17:04:29 -0500

    Added JS for table + more styling

--- js/main.js
+++ js/main.js
@@ -1,24 +1,33 @@
 async function getJSON() {
     lap = document.getElementsByName('lap')[0].value
+    year = document.getElementsByName('year')[0].value
+    // position = MRData.StandingsTable.StandingsLists[0].DriverStandings[0].position
+    // name = MRData.StandingsTable.StandingsLists[0].DriverStandings[0].Driver.familyName
+    // nationality = MRData.StandingsTable.StandingsLists[0].DriverStandings[0].Driver.nationality
+    // points = MRData.StandingsTable.StandingsLists[0].DriverStandings[0].points
+
     console.log(lap)
-    await fetch(`https://ergast.com/api/f1/2019/${lap}/driverStandings.json`)
+    await fetch(`https://ergast.com/api/f1/${year}/${lap}/driverStandings.json`)
         .then(data => data.json())
         .then(rawData => {
-            let racers = rawData.MRData.total
-            //let main_data = 
-            for(let i = 0; i <3; i++){
-                let constructor_name = rawData.MRData.StandingsTable.StandingsLists[0].DriverStandings[i].Constructors[0].name
-                display_constructors = document.createElement('h1')
-                display_constructors.innerHTML = constructor_name
-                document.body.append(display_constructors)
-            }
-            display_racers = document.createElement('h3')
-            if (!racers) {
-                display_racers.innerHTML = "LOADING..."
-                document.body.append(display_racers)
-            } else {
-                display_racers.innerHTML = racers
-                document.body.append(display_racers)
+            for(let i = 0; i < 7; i++){
+
+                let position_number = rawData.MRData.StandingsTable.StandingsLists[0].DriverStandings[i].position
+                let name = rawData.MRData.StandingsTable.StandingsLists[0].DriverStandings[i].Driver.familyName
+                let nationality = rawData.MRData.StandingsTable.StandingsLists[0].DriverStandings[i].Driver.nationality
+                 let constructor_name = rawData.MRData.StandingsTable.StandingsLists[0].DriverStandings[i].Constructors[0].name
+                 let points = rawData.MRData.StandingsTable.StandingsLists[0].DriverStandings[i].points
+                document.getElementById(`position${i.toString()}`).innerHTML = position_number
+                document.getElementById(`name${i.toString()}`).innerHTML = name
+                document.getElementById(`nationality${i.toString()}`).innerHTML = nationality
+                 document.getElementById(`sponsor${i.toString()}`).innerHTML = constructor_name
+                 document.getElementById(`points${i.toString()}`).innerHTML = points
+                document.body.append(position_number)
+                document.body.append(name)
+                document.body.append(nationality)
+                document.body.append(constructor_name)
+                document.body.append(points)
+
             }
         })
         .catch(error => {
